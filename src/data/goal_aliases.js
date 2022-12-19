e6b7a2abba5ff8ea27861c04d46bcf29cc45ab67
Julian Bock von Wülfingen <jwuelfingen@techfak.de>
2022-12-19 16:26:01 +0100

    plan ideas

--- src/data/goal_aliases.js
+++ src/data/goal_aliases.js
@@ -2,4 +2,12 @@ export const MINING_LEVEL_WOOD    = 0
 export const MINING_LEVEL_STONE   = 1
 export const MINING_LEVEL_IRON    = 2
 export const MINING_LEVEL_DIAMOND = 3
-export const ENCHANTING_ACCESS    = 4
+export const ENCHANTING_ACCESS    = 4 // Fortune Enchantment
+export const BEACON_ACCESS        = 5 // Less Stone Weight
+export const TUNNEL_BORE_ACCESS   = 6 // Mine 4 Blocks
+
+// Tree Farm (Wood)
+// Stone Farm
+// Iron Golem Farm (Iron)
+// Witch Farm (Redstone)
+// Query (All)
