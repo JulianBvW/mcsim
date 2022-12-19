7b4fb9e63bf55e11e57c567c95b35a9fb78a31ad
Julian Bock von Wülfingen <jwuelfingen@techfak.de>
2022-12-19 11:38:42 +0100

    added some goals

---
+++ src/data/goals.js
@@ -0,0 +1,27 @@
+export default [
+    {
+        id: 0,
+        name: 'Stone Pickaxe',
+        image: 'pickaxe-stone',
+        requires: {
+            'stone': 3
+        }
+    },
+    {
+        id: 1,
+        name: 'Iron Pickaxe',
+        image: 'pickaxe-iron',
+        requires: {
+            'iron': 3,
+            'coal': 1
+        }
+    },
+    {
+        id: 2,
+        name: 'Diamond Pickaxe',
+        image: 'pickaxe-diamond',
+        requires: {
+            'diamond': 3
+        }
+    }
+]
--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore } from 'vuex'
 import resources from '@/data/resources'
+import goals from '@/data/goals'
 
 export default createStore({
     state: {
         resources: resources,
+        goals: goals,
         inventory: {}
     },
     getters: {
