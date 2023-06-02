b0d1f640ae0bdeceb6c351f86a71033676345867
Julian Bock von Wülfingen <jwuelfingen@techfak.de>
2023-06-02 19:39:48 +0200

    Added Vue CLI PWA

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import BootstrapVue3 from 'bootstrap-vue-3'
 // Import Bootstrap and BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
+import './registerServiceWorker'
 
 
 createApp(App).use(store).use(router).use(BootstrapVue3).mount('#app')
